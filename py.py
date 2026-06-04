c4bbb85f594d7565b795fe8ba85a0a4021653ddb
Patriarch-19 <alecjeremy19@gmail.com>
2026-06-04 10:55:53 +0100

    Prototype

--- py.py
+++ py.py
@@ -1,8 +1,76 @@
 # print("Hello, World")
-var1 = "Obi is a boy."
-print(var1)
+# var1 = "Obi is a boy."
+# print(var1)
 
-name = "Ada"
-addition = 5 + 6
-print(name)
-print(addition)
+# name = "Ada"
+# addition = 5 + 6
+# print(name)
+# print(addition)
+
+# complex = 1 + 2j
+# print(complex)
+
+# x = "Obi"
+# y = type(x)
+# print(y)
+# x = 25.5
+# print(x)
+# print(type(x))
+
+# var1 = "A"
+# print(ord(var1))
+# var2 = "a"
+# print(ord(var2))
+
+# xx = str(x)
+# print(xx)  
+# print(type(xx))
+
+# x, y, z = "Orange", "Banana", "Cherry"
+# print(x)
+# print(y)
+# print(z)
+
+darthVader = "I have the high ground."
+# for i in darthVader:
+#      print(i)
+    
+# print(len(darthVader))
+vader = " Obi is a boy "
+# print(len(vader))
+
+# print("boy" in vader)
+# print("father" in vader)
+# print("Anakin" in darthVader)
+# print("gun" in darthVader)
+# print(vader[0:3])
+# print(vader[:3])
+# print(vader[1:6])
+# print(vader[2:])
+# print(vader[1:8])
+
+var = "InformationTechnology"
+print(var[1:8:2])
+print(var[1::3])
+
+myList = ["Orange", "Apple", "Mango", "Banana", "Grapes", "Cucumber", "Papaya", "Pineapple", "Watermelon", "Strawberry"]
+print(myList[4])
+print(myList[4][0])
+
+"""
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
+reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
+sunt in culpa qui officia deserunt mollit anim id est laborum.
+"""
+
+print(vader.upper())
+print(vader.lower())
+print(vader.capitalize())
+print(vader.title())
+print(vader.strip())
+a = "Obi is a boy "
+b = "Ada is a girl."
+print(a + " " + b)
+
